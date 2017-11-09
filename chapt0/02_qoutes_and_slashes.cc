0e4f98b96a66a70415d4cd649b0e86068239c41d
Canta Tronic <tokareva@jinr.ru>
2017-11-10 00:42:10 +0300

    Add simple run.sh for compiling and make a first couple of excercises

---
+++ chapt0/02_qoutes_and_slashes.cc
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	cout<<"This (\") is a qoute, and this (\\) is a backslash."<<endl;
+	return 0;
+}
+
---
+++ chapt0/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ -O2 -g -Wall $1 -o code
+./code
+
