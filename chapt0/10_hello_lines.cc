adde13b94e505db62d0ebbd182b6deaee73476fe
Canta Tronic <tokareva@jinr.ru>
2017-11-10 01:37:59 +0300

    Add the solution for ex.10

---
+++ chapt0/10_hello_lines.cc
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+using 
+std
+::
+cout
+;
+using 
+std
+::
+endl
+;
+
+int
+ main
+(
+)
+{
+	//interesting stuff //
+	//in comments with /*
+	//and other things */ about ex.8
+	cout
+<<
+"Hello, world!"
+<<
+endl
+;
+	return
+ 0
+;
+}
+
