da62aca74d7a5226ca9085f7242b3f888c737978
Canta Tronic <tokareva@jinr.ru>
2017-11-10 02:32:12 +0300

    Add the solution for the ex.3, ch. 1

---
+++ chapt1/13_non-valid_strings.cc
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+/*
+This pogram is not valid... For some reasons.
+*/
+
+int main(){
+	{const string s = "a string";
+	cout<s<<endl;}
+
+	{const string s = "another one string";
+	cout<<s<<endl;}
+
+	return 0;
+}
+
