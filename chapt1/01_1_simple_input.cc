cb8d2f06a3934e9f16e7063ea078812165d761d9
Canta Tronic <tokareva@jinr.ru>
2017-11-10 01:55:24 +0300

    Add the first solution for the ex.0, ch.1, and run.sh moved to project root folder

---
+++ chapt1/01_1_simple_input.cc
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+
+using std::cout; 		using std::cin;
+using std::endl;		using std::string;
+
+int main(){
+	cout<<"Please, enter your name: ";
+	string name;
+	cin>>name;
+	cout<<"Hello, "<<name<<"! Nice to see you."<<endl;
+	return 0;
+}
+
