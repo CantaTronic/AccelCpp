f6532081dd560deb2334c7a1f354aa773dd5ae5f
Canta Tronic <tokareva@jinr.ru>
2017-11-11 22:26:47 +0300

    Add the solution for the ex. 1, ch. 2

---
+++ chapt2/20_name_with_frame.cc
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+
+int main(){
+	cout<<"Please, enter your first name: ";
+	string name;
+	cin>>name;
+
+	//building message we intend to write
+	string greeting = "Hello " + name + "!";
+
+	//the number of pads suppounding the greeting
+	const int pad = 1;
+
+	//the number of rows and columns to write
+	const int rows = 2*pad + 3;
+	const string::size_type cols = greeting.size() + 2*pad + 2;
+	
+	cout<<endl;
+
+	//invariant: we have written r rows so far
+	for (int r = 0; r != rows; r++){
+	string::size_type c = 0;
+	
+	//invariant: we have c characters written in the current row so far
+		while (c != cols){
+			//is it time to write the greeting?
+			if (r == pad + 1 && c == pad + 1){
+				cout<<greeting;
+				c += greeting.size();
+		
+			} else {
+				//are we on the border?
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) 
+					cout<<"*";
+				else 
+					cout<<" ";
+				c++;
+			}
+
+		}
+		cout<<endl;	
+	}	
+	return 0;
+}
+
