15733f06bd8e72c285e1dcfaccb588d0c6a5f854
Canta Tronic <tokareva@jinr.ru>
2017-11-12 22:41:47 +0300

    Add the solution for the ex.6, ch. 2

---
+++ chapt2/26_count.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//This small program counts from 1 to 10
+
+int main(){
+	int = 0;
+	while (i < 10){
+		i+=1;
+		cout<<i<<endl;
+	}
+	return 0;
+}
+
