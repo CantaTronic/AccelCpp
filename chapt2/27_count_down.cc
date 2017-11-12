c3c2c946bf76deea67bfbd1ffbdea50a9ec93cc4
Canta Tronic <tokareva@jinr.ru>
2017-11-12 22:45:43 +0300

    Add the solution for the ex.7, ch. 2

---
+++ chapt2/27_count_down.cc
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//This small program counts from 1 to 10
+
+int main(){
+	for (int i = 10; i >= -5; i--){
+		cout<<i<<endl;
+	}
+	return 0;
+}
+
