14cef831d5bea05ffccd30417e0d4da9306f6f16
Canta Tronic <tokareva@jinr.ru>
2017-11-26 23:59:48 +0300

    Add read_input.h

---
+++ chapt4/4_5_ex/read_input.h
@@ -0,0 +1,11 @@
+
+#ifndef GUARD_read_input_h
+#define GUARD_read_input_h
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::vector<std::string> read_words(std::istream &in);
+
+#endif
