7ece6940dabee394076d6b4c3fdf41570119db2a
Canta Tronic <tokareva@jinr.ru>
2017-11-27 00:06:13 +0300

    Add read_input.cc

---
+++ chapt4/4_5_ex/read_input.cc
@@ -0,0 +1,17 @@
+
+#include "read_input.h"
+
+using std::vector; 
+using std::istream; 
+using std::string;  
+
+vector<string> read_input::read_words(istream &in){
+    string tmp;
+    vector<string> vect;
+    in>>tmp;
+    while (tmp != "0") {
+        vect.push_back(tmp);
+        in>>tmp;
+    }
+    return vect;
+}
