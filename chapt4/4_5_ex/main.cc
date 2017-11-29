c6cc131325e39e96751389616d61bce21b8a77f1
Canta Tronic <tokareva@jinr.ru>
2017-11-29 22:21:44 +0300

    Rewtire the solution to escape printing of the same words many time. Now this is the complete solution for ex. 5, ch. 4

--- chapt4/4_5_ex/main.cc
+++ chapt4/4_5_ex/main.cc
@@ -4,12 +4,14 @@
 #include <string>
 #include <iterator>
 #include <algorithm>
+#include <set>
 #include "read_input.h"
 
 using std:: cin;    using std::vector;
 using std::cout;    using std::endl;
 using std::begin;   using std::string;  
 using std::end;     using std::count;
+using std::set;
 
 
 int main(){
@@ -18,9 +20,14 @@ int main(){
     cout<<"================="<<endl;
     const vector<string>::size_type size = my_vect.size();
     cout<<"There are "<<size<<" words in your text"<<endl;
+    cout<<"This is how often any of words is occured:"<<endl;
+    set<string> my_set;
     for (unsigned i = 0; i<size; i++){
-		cout<<"\t"<<my_vect[i]<<": "<<count(begin(my_vect), end(my_vect), my_vect[i])<<endl;
+        if ( my_set.find(my_vect[i]) == end(my_set)) {
+            my_set.insert(my_vect[i]);
+            cout<<"\t"<<my_vect[i]<<": "<<count(begin(my_vect), end(my_vect), my_vect[i])<<endl;
+        }
 	}
-
+    cout<<"So, there are "<<my_set.size()<<" unique words"<<endl;
     return 0;
 }
