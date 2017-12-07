3dcbc05b4f4c78686983a1d66c4236d7659784d7
Canta Tronic <tokareva@jinr.ru>
2017-12-07 23:16:11 +0300

    Move user records managing from vectors to lists

--- chapt5/5_0_vector_search/Student_info.cc
+++ chapt5/5_0_vector_search/Student_info.cc
@@ -2,8 +2,10 @@
 //source file for Student_info-related functions
 #include "Student_info.h"
 #include "grade.h"
+#include <list>
 
 using std::istream; using std::vector;
+using std::list;
 
 bool compare (const Student_info &x,const Student_info &y){
     return x.name<y.name;
@@ -43,10 +45,24 @@ bool fgrade (const Student_info & s) {
 //   return fail;
 // }
 
-//the third variant, even more based on iterators
-vector<Student_info> extract_fails(vector<Student_info>& students)  {
-  vector<Student_info> fail;
-  vector<Student_info>::iterator iter = students.begin();
+// //the third variant, even more based on iterators
+// vector<Student_info> extract_fails(vector<Student_info>& students)  {
+//   vector<Student_info> fail;
+//   vector<Student_info>::iterator iter = students.begin();
+//   while(iter != students.end()) {
+//     if (fgrade(*iter)){
+//       fail.push_back(*iter);
+//       iter = students.erase(iter);
+//     } else
+//       iter++;
+//   }
+//   return fail;
+// }
+
+//the 4th variant, which uses the list container
+list<Student_info> extract_fails(list<Student_info>& students)  {
+  list<Student_info> fail;
+  list<Student_info>::iterator iter = students.begin();
   while(iter != students.end()) {
     if (fgrade(*iter)){
       fail.push_back(*iter);
@@ -56,6 +72,7 @@ vector<Student_info> extract_fails(vector<Student_info>& students)  {
   }
   return fail;
 }
+
 
 bool read (istream& is, Student_info& s) {
     //read and store the student's name and midterm and final exam grades
