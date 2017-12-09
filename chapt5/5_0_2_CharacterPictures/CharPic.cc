d8aae0c72cfbada4a2f627b90ee45d939488121e
Canta Tronic <tokareva@jinr.ru>
2017-12-09 23:18:00 +0300

    Add and test framing fuction

--- chapt5/5_0_2_CharacterPictures/CharPic.cc
+++ chapt5/5_0_2_CharacterPictures/CharPic.cc
@@ -7,9 +7,18 @@ using std::string;  using std::vector;
 using std::cout;    using std::endl;
 
 string::size_type width (const vector<string> & vect);
+vector<string> frame (const vector<string> & vect);
+void printVec (const vector<string> & vect);
 
 int main() {
-  
+  vector<string> testVect, vect2;
+  testVect.push_back("Test");
+  testVect.push_back("string");
+  cout<<"Without frame: "<<endl;
+  printVec(testVect);
+  vect2 = frame (testVect);
+  cout<<"With frame: "<<endl;
+  printVec(vect2);
   return 0;
 }
 
@@ -23,4 +32,24 @@ string::size_type width (const vector<string> & vect) {
     }
   }
   return maxlen;
+}
+
+vector<string> frame (const vector<string> & vect) {
+  vector<string> ret;
+  string::size_type maxlen = width(vect);
+  string border(maxlen + 4,'*');
+  ret.push_back(border);
+  for (vector<string>::size_type i = 0;  i != vect.size(); i++) {
+    string tmp = "* " + vect[i] + string(maxlen - vect[i].size() + 1, ' ') + '*';
+    ret.push_back(tmp);
+  }
+  ret.push_back(border);
+  return ret;
+}
+
+void printVec (const vector<string> & vect) {
+  /*Print out the vector*/
+    for (vector<string>::size_type i = 0; i != vect.size(); i++) {
+        cout<<vect[i]<<endl;
+    }
 }
