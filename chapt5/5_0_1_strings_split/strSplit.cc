126aa1038d44a24ff8152a3f75db5af282350e4b
Canta Tronic <tokareva@jinr.ru>
2017-12-09 22:23:23 +0300

    Add and test sol. for ex.0.1, ch. 5

--- chapt5/5_0_1_strings_split/strSplit.cc
+++ chapt5/5_0_1_strings_split/strSplit.cc
@@ -1,9 +1,65 @@
 #include <cctype>
 #include <vector>
 #include <string>
+#include <iostream>
+// #include "gtest/gtest.h"   //I'm just too lazyto link this small training project with gtest. Shame on me =))
 
-using std::istream; using std::vector;
-using std::list;    using std::string;
+using std::vector;  using std::string;
+using std::cout;    using std::endl;
+
+vector<string> split (const string& s);   //actual function
+
+/*and a bit of funny stuff for testing :)*/
+bool operator== (const vector<string> & x, const vector<string> & y); 
+void testVectorEquality();
+void testSplit();
+
+int main () {
+  testVectorEquality();
+  testSplit();
+  return 0; 
+}
+
+void testVectorEquality() {
+  vector<string> a(4);
+  vector<string> b;
+  cout<<"(a==a) is "<<(a==a)<<endl;   //EXPECT_EQ((a==a),1);
+  cout<<"(a==b) is "<<(a==b)<<endl;   //EXPECT_EQ((a==b),0);
+}
+
+bool operator== (const vector<string> & x, const vector<string> & y){
+  if (x.size() != y.size()) {
+    return false;
+  } else {
+    const vector<string>::size_type v_size = x.size();
+    bool equal = true; 
+    vector<string>::size_type i = 0;
+    while (equal && i != v_size) {
+      equal = (x[i] == y [i]);
+      i++;
+    }
+    return equal;
+  }
+}
+
+void testSplit() {
+    vector<string> myVect = split("This is a phrase for test splitting");
+    vector<string> testVect;
+    testVect.push_back("This");
+    testVect.push_back("is");
+    testVect.push_back("a");
+    testVect.push_back("phrase");
+    testVect.push_back("for");
+    testVect.push_back("test");
+    testVect.push_back("splitting");
+//     EXPECT_EQ(1,(myVect == testVect));
+    if (myVect == testVect) {
+      cout<<"Split works"<<endl;
+    } else {
+      cout<<"Split doesn't work"<<endl;
+    }
+	
+}
 
 vector<string> split (const string& s) {
   vector<string> ret;
